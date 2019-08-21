7f624a6e049dcb4f8cc37fb2249613e855f51ead
Jeffy <JerryHuang@sinbon.com>
2019-08-21 09:31:15 +0800

    1080812:1.0

--- backend.py
+++ backend.py
@@ -12,7 +12,7 @@ import numpy as np
 from enum import Enum
 
 
-class Machine_Status(Enum):
+class en_machine_status(Enum):
     stop =1
     moving_down =2
     modeling =3
@@ -20,133 +20,129 @@ class Machine_Status(Enum):
     finished =5
     system_error = 6
 
-#set window
-#set RaspberryyPi
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-# Define GPIO to use on Pi
-moinitor_pin = 26
-trigger_pin = 23
-echo_pin = 24
-GPIO.setup(moinitor_pin, GPIO.OUT)
-GPIO.setup(trigger_pin,GPIO.OUT)  # Trigger
-GPIO.setup(echo_pin,GPIO.IN)      # Echo
-v=343 #331+0.6T, T=Celsius
-#set machine_flag  to stop 
-machine_flag = Machine_Status.stop
-start_time =0
-end_time =0   
+class Distancer:
+    def __init__(self,moinitor_pin,trigger_pin,echo_pin):
+        #set window
+        #set RaspberryyPi
+        self.moinitor_pin = moinitor_pin
+        self.trigger_pin = trigger_pin
+        self.echo_pin = echo_pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        # Define GPIO to use on Pi
+        GPIO.setup(self.moinitor_pin, GPIO.OUT)
+        GPIO.setup(self.trigger_pin,GPIO.OUT)  # Trigger
+        GPIO.setup(self.echo_pin,GPIO.IN)      # Echo
+        self.v=343 #331+0.6T, T=Celsius
+        self.machine_flag = en_machine_status.stop
+        self.start_time =0
+        self.end_time =0
+        self.stop_flag = False
+        self.conn = pymssql.connect(host='192.168.0.56',
+                        user='sa',
+                        password='1234',
+                        database='Raspberry_pi')
+        self.cur=self.conn.cursor()
+    def insert(self,wo_id="",ma_id="",lm_user="",op="",count="",start_time="",end_time="",lm_time=""):
+        sql = "INSERT INTO [Raspberry_pi].[dbo].[distance] (wo_id,lm_user,op,count,start_time,end_time,lm_time,ma_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+        params = (wo_id,lm_user,op,count,start_time,end_time,lm_time,ma_id)
+        self.cur.execute(sql,params)
+        self.conn.commit()
+    def loggingwarning(self,message):
+        logging.basicConfig(filename='logtest.txt',  filemode='a',format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+        logging.warning(message)
+    def stop_work(self):
+        self.stop_flag = True
+    def start_work(self):
+        self.stop_flag = False
+    #send_trigger_pulse must wait until the echo pin goes high and then times how long the echo pin stays high.
+    def send_trigger_pulse(self):
+        GPIO.output(self.trigger_pin, True)
+        time.sleep(0.0001)
+        GPIO.output(self.trigger_pin, False)
+    #value waits until the echo pin either goes high or low
+    #timeout argument is used to provide a timeout so that if for anyreason
+    def wait_for_echo(self,value, timeout):
+        count = timeout
+        while GPIO.input(self.echo_pin) != value and count > 0:
+            count = count - 1
+        #if timeout --> system_error
+        if(count == 0):
+            self.machine_flag == en_machine_status.stop
+        return count
+    def get_distance(self):
+        self.send_trigger_pulse()
+        self.wait_for_echo(True, 5000)
+        #print("start wait_for_echo %s" % wait_for_echo(True, 5000))
+        start = time.time()
+        self.wait_for_echo(False, 5000)
+        #print("finish wait_for_echo %s" % wait_for_echo(False, 5000))
+        finish = time.time()
+        pulse_len = finish - start
+        #distance pulse travelled in that time is pulse_len
+        #multiplied by the speed of sound (cm/s) v=3s43 #331+0.6T, T=Celsius
+        #That was the distance there and back so halve the value
+        distance_cm = ((pulse_len * self.v)/2 )*100
+        return int(distance_cm)
 
-def insert(wo_id,lm_user,op,count,start_time,end_time,lm_time,ma_id):
-    conn = pymssql.connect(host='192.168.0.56',
-                       user='sa',
-                       password='1234',
-                       database='Raspberry_pi')
-    cur=conn.cursor()
-    sql = "INSERT INTO [Raspberry_pi].[dbo].[distance] (wo_id,lm_user,op,count,start_time,end_time,lm_time,ma_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-    params = (wo_id,lm_user,op,count,start_time,end_time,lm_time,ma_id)
-    cur.execute(sql,params)
-    conn.commit()
-    conn.close()
+    def detection_most_frequent_destence(self):
+        List = []
+        for i in range(0,3,1):
+            List.append(self.get_distance())
+            time.sleep(0.02)
+        return max(set(List), key = List.count)
+        #return self.get_distance()
 
-def loggingwarning(message):
-    logging.basicConfig(filename='logtest.txt',  filemode='a',format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
-    logging.warning(message)             
+    def get_time_strformat(self):
+        x= datetime.datetime.now()
+        return x.strftime("%Y-%m-%d %H:%M:%S")
+    def get_datetime_strptime(self,value):
+        return datetime.datetime.strptime(value, '%m/%d/%y %H:%M:%S')
+    def sorted_reversed(self,value):
+        if sorted(value) == list(value):
+            return "up"
+        elif list(reversed(sorted(value))) == list(value):
+            return "down"
+        else:
+            return "none"
 
-
-    
-    
-#send_trigger_pulse must wait until the echo pin goes high and then times how long the echo pin stays high.
-def send_trigger_pulse():
-    GPIO.output(trigger_pin, True)
-    time.sleep(0.0001)
-    GPIO.output(trigger_pin, False)
-
-#value waits until the echo pin either goes high or low
-#timeout argument is used to provide a timeout so that if for anyreason
-def wait_for_echo(value, timeout):
-    count = timeout
-    while GPIO.input(echo_pin) != value and count > 0:
-        count = count - 1
-    #if timeout --> system_error
-    if(count == 0):
-        global sys_flag
-        sys_flag = Machine_Status.system_error
-    return count
-
-def get_distance():
-    send_trigger_pulse()
-    wait_for_echo(True, 5000)
-    #print("start wait_for_echo %s" % wait_for_echo(True, 5000))
-    start = time.time()
-    wait_for_echo(False, 5000)
-    #print("finish wait_for_echo %s" % wait_for_echo(False, 5000))
-    finish = time.time()
-    pulse_len = finish - start
-    #distance pulse travelled in that time is pulse_len
-    #multiplied by the speed of sound (cm/s) v=3s43 #331+0.6T, T=Celsius
-    #That was the distance there and back so halve the value
-    distance_cm = ((pulse_len *v)/2 )*100
-    return int(distance_cm)
-
-def detection_most_frequent_destence():
-    List = []
-    for i in range(0,5,1):
-        List.append(get_distance())
-        time.sleep(0.01)
-    return max(set(List), key = List.count)
-
-def get_time_strformat():
-    x= datetime.datetime.now()
-    return x.strftime("%Y-%m-%d %H:%M:%S")
-
-def sorted_reversed(value):
-    if sorted(value) == list(value):
-        return "up"
-    elif list(reversed(sorted(value))) == list(value):
-        return "down"
-    else:
-        return "none"
-
-def detect_working(wo_id,ma_id,lm_user,op,count):
-    global machine_flag
-    #suspend_time = 0
-    global start_time, end_time
-    Li_result =[]
-    while True:
-        t_detect_1 = detection_most_frequent_destence()
-        print("The t_detect_1 is %dcm." % t_detect_1)
-        time.sleep(1)
-        t_detect_2 =  detection_most_frequent_destence()
-        print("The t_detect_2 is %dcm." % t_detect_2)
-        time.sleep(1)
-        t_detect_3 =  detection_most_frequent_destence()
-        print("The t_detect_3 is %dcm." % t_detect_3)
-        # deal some unnormal issuses
-        if(np.std([t_detect_1,t_detect_2,t_detect_3]) > 10):
-            machine_flag= Machine_Status.stop
-        if(np.std([t_detect_1,t_detect_2,t_detect_3]) <= 1 and machine_flag == Machine_Status.stop):
-            start_time = get_time_strformat()
-            print("Machine  %s" % Machine_Status(machine_flag))
-            print("start working and the starttime is %s" %start_time)
-        elif( sorted_reversed([t_detect_1,t_detect_2,t_detect_3]) == "down" and 3 < np.std([t_detect_1,t_detect_2,t_detect_3]) < 7.5):
-            machine_flag= Machine_Status.moving_down
-            print("Machine  %s" % Machine_Status(machine_flag))
-        elif( sorted_reversed([t_detect_1,t_detect_2,t_detect_3]) == "up" and machine_flag == Machine_Status.moving_down
-        and 3 < np.std([t_detect_1,t_detect_2,t_detect_3]) < 7.5):
-            machine_flag= Machine_Status.moving_up
-            print("Machine  %s" % Machine_Status(machine_flag))
-        elif(np.std([t_detect_1,t_detect_2,t_detect_3]) <= 1 and machine_flag == Machine_Status.moving_up):
-            end_time = get_time_strformat()
-            print("finish work and the starttime is %s" %start_time)
-            print("finish work and the endtime is %s" %end_time)
-            if(start_time !=0):
-                message = "starttime:{0} - endtime:{1}".format(start_time,end_time)
-                #loggingwarning(message)
-                Li_result.append(message)
-                print("transation commit")
-                
-            machine_flag= Machine_Status.stop
-            start_time=0
-            end_time = 0
-              
+    def detect_working(self,data,queue): 
+        while True and not self.stop_flag:
+            t_detect_1 = self.detection_most_frequent_destence()
+            time.sleep(1)
+            t_detect_2 =  self.detection_most_frequent_destence()
+            time.sleep(1)
+            t_detect_3 =  self.detection_most_frequent_destence()
+            print("de1: is %dcm. de2: %dcm. de3: %dcm." %  (t_detect_1,t_detect_2,t_detect_3))
+            # deal some unnormal issuses
+            if(np.std([t_detect_1,t_detect_2,t_detect_3]) > 10 or np.std([t_detect_1,t_detect_3]) ==0 
+            or np.std([t_detect_1,t_detect_2]) ==0 or np.std([t_detect_2,t_detect_3]) ==0):
+                self.machine_flag= en_machine_status.stop
+            if(np.std([t_detect_1,t_detect_2,t_detect_3]) <= 1 and self.machine_flag == en_machine_status.stop):
+                self.start_time = self.get_time_strformat()
+                print("Machine  %s" % en_machine_status(self.machine_flag))
+                print("start working and the starttime is %s" % self.start_time)
+            elif( self.sorted_reversed([t_detect_1,t_detect_2,t_detect_3]) == "down" and np.std([t_detect_1,t_detect_2,t_detect_3]) > 2):
+                self.machine_flag= en_machine_status.moving_down
+                print("Machine  %s" % en_machine_status(self.machine_flag))
+            elif( self.sorted_reversed([t_detect_1,t_detect_2,t_detect_3]) == "up" and self.machine_flag == en_machine_status.moving_down
+            and np.std([t_detect_1,t_detect_2,t_detect_3]) > 2):
+                self.machine_flag= en_machine_status.moving_up
+                print("Machine  %s" % en_machine_status(self.machine_flag))
+            elif(np.std([t_detect_1,t_detect_2,t_detect_3]) <= 1 and self.machine_flag == en_machine_status.moving_up):
+                self.end_time = self.get_time_strformat()
+                print("finish work and the starttime is %s" % self.start_time)
+                print("finish work and the endtime is %s" % self.end_time)
+                if self.start_time !=0:
+                    if (self.get_datetime_strptime(self.end_time) - self.get_datetime_strptime(self.start_time)).seconds() > 8:
+                        message = "starttime:{0} - endtime:{1}".format(self.start_time,self.end_time)
+                        #loggingwarning(message)
+                        self.insert(wo_id= data[0],ma_id=data[1],lm_user=data[2],op=data[3],count=data[4],start_time= self.start_time,end_time=self.end_time,lm_time=self.end_time)
+                        queue.put(message)
+                        #print("transation commit")   
+                self.machine_flag= en_machine_status.stop
+                self.start_time = 0
+                self.end_time = 0
+    def __del__(self):
+        self.conn.close()
+        GPIO.cleanup()
--- main.py
+++ main.py
@@ -1,88 +1,126 @@
 #! /usr/bin/env python3
 # coding:utf-8
-import tkinter as tk
-import threading
-import os   
-import backend
 
+from tkinter import *
+import threading,os,time,queue
+from backend import Distancer
+from tkinter import messagebox
+
+distancer = Distancer(moinitor_pin= 26,trigger_pin= 23,echo_pin= 24)
 class detectWindow():
+    def __init__(self,window):
+        self.stop_report_flag = False
+        self.que = queue.Queue(maxsize = 100)  
+        self.window = window
+        self.window.wm_title("Working Window")
+        l1=Label(window,text="機台:",font=("Courier", 30))
+        l1.grid(row=0,column=0)
+        l2=Label(window,text="工單:",font=("Courier", 30))
+        l2.grid(row=0,column=2)
+        l3=Label(window,text="工號:",font=("Courier", 30))
+        l3.grid(row=1,column=0)
+        l4=Label(window,text="製程:",font=("Courier", 30))
+        l4.grid(row=1,column=2)
+        l4=Label(window,text="穴數:",font=("Courier", 30))
+        l4.grid(row=2,column=0)
+        self.l5=Label(window,text="停止",font=("Courier", 45))
+        self.l5['fg'] ="red"
+        self.l5.grid(row=3,column=3)
+        machine_id=StringVar()
+        self.e1=Entry(window,textvariable=machine_id,font=("Courier", 24))
+        self.e1.grid(row=0,column=1)
+        wo_id=StringVar()
+        self.e2=Entry(window,textvariable=wo_id,font=("Courier", 24))
+        self.e2.grid(row=0,column=3)
+        wo_num=StringVar()
+        self.e3=Entry(window,textvariable=wo_num,font=("Courier", 24))
+        self.e3.grid(row=1,column=1)
+        op=StringVar()
+        self.e4=Entry(window,textvariable=op,font=("Courier", 24))
+        self.e4.grid(row=1,column=3)     
+        count=StringVar()
+        self.e5=Entry(window,textvariable=count,font=("Courier", 24))
+        self.e5.grid(row=2,column=1)
+        #POM2FA08
+        #molding
+        #MNG_970067
+        #2
+        #093173
+        self.e1.insert(END, "POM2FA08")
+        self.e2.insert(END, "MNG_970067")
+        self.e3.insert(END, "093173")
+        self.e4.insert(END, "molding")
+        self.e5.insert(END, "2")
+        self.list1 = Listbox(window, height=20,width=60)
+        self.list1.grid(row=3,column=0,rowspan=6,columnspan=2)
+        sb1= Scrollbar(window)
+        sb1.grid(row=3,column=2,rowspan=10)
+        sb1.configure(command= self.list1.yview)
+        self.b1=Button(window,text="開始", width=20,font=("Courier", 16),command=self.detect_working)
+        self.b1.grid(row=0,column=6)
+        b2= Button(window,text="結束", width=20,font=("Courier", 16),command=self.end_command)
+        b2.grid(row=1,column=6)
+        b3 = Button(window,text="鍵盤",width=20,font=("Courier", 16),command=self.callKeyboard)
+        b3.grid(column=6,row=3)
     def detect_working(self):
-        while True:
-            wo_id =self.e2.get()
-            ma_id = self.e1.get()
-            lm_user = self.e3.get()
-            op =  self.e4.get()
-            count =self.e5.get()
-            backend.detect_working(wo_id,ma_id,lm_user,op,count)
+        wo_id = self.e2.get()
+        ma_id = self.e1.get()
+        lm_user = self.e3.get()
+        op =  self.e4.get()
+        count =self.e5.get()
+        if not wo_id and not ma_id and not lm_user and not op and not count:
+            messagebox.showwarning("Warning","請輸入資料!")
+        else:
+            self.disabledFunctions()
+            data = (wo_id,ma_id,lm_user,op,count)
+            distancer.start_work()
+            self.stop_report_flag = False
+            self.t = threading.Thread(target=distancer.detect_working, args=(data, self.que))
+            self.t.start()
+            self.t2 = threading.Thread(target=self.report_detect_result)
+            self.t2.start()
+    def disabledFunctions(self):
+        self.b1['state'] = 'disabled'
+        self.e1['state'] = 'disabled'
+        self.e2['state'] = 'disabled'
+        self.e3['state'] = 'disabled'
+        self.e4['state'] = 'disabled'
+        self.e5['state'] = 'disabled'
+        self.l5['text'] = "執行中...."
+        self.l5['fg'] ="green"
+    def enabledFunctions(self):
+        self.b1['state'] = 'normal'
+        self.e1['state'] = 'normal'
+        self.e2['state'] = 'normal'
+        self.e3['state'] = 'normal'
+        self.e4['state'] = 'normal'
+        self.e5['state'] = 'normal'
+        self.l5['text'] = "停止"
+        self.l5['fg'] ="red"
     def keyboard(self):
         os.system('florence')
-    def add_command(self):
-        self.list1.insert(tk.END,(self.e1.get()))
+    def report_detect_result(self):
+        ls_index = 1
+        processdot = 1
+        while True and not self.stop_report_flag:
+            #print(self.que.qsize())
+            time.sleep(1)
+            self.l5['text'] = "執行中%s" %  ('.' * processdot)
+            if processdot == 4:
+                processdot =0
+            else:
+                processdot += 1
+            if self.que.qsize() != 0:
+                self.list1.insert(ls_index,self.que.get())
+                ls_index = ls_index +1
+            ls_index =1 if ls_index == 20 else False
+        self.enabledFunctions()
+    def end_command(self):
+        distancer.stop_work()
+        self.stop_report_flag = True
     def callKeyboard(self):
-    #global lock
-    #lock.acquire()
         added_thread = threading.Thread(target=self.keyboard)
-        added_thread.start()
-        #lock.release()
-    def window_initial(self):
-        window = tk.Tk()
-        window.wm_title('my window')
-        #window.geometry('2000x3000')
-        l1=tk.Label(window,text="機台:",font=("Courier", 45))
-        l1.grid(row=0,column=0)
-
-        l2=tk.Label(window,text="工單:",font=("Courier", 45))
-        l2.grid(row=0,column=2)
-
-        l3=tk.Label(window,text="工號:",font=("Courier", 45))
-        l3.grid(row=1,column=0)
-
-        l4=tk.Label(window,text="製程:",font=("Courier", 45))
-        l4.grid(row=1,column=2)
-
-        l4=tk.Label(window,text="穴數:",font=("Courier", 45))
-        l4.grid(row=2,column=0)
-        
-        machine_id=tk.StringVar()
-        self.e1=tk.Entry(window,textvariable=machine_id,font=("Courier", 24))
-        self.e1.grid(row=0,column=1)
-
-        wo_id=tk.StringVar()
-        self.e2=tk.Entry(window,textvariable=wo_id,font=("Courier", 24))
-        self.e2.grid(row=0,column=3)
-
-        wo_num=tk.StringVar()
-        self.e3=tk.Entry(window,textvariable=wo_num,font=("Courier", 24))
-        self.e3.grid(row=1,column=1)
-
-        op=tk.StringVar()
-        self.e4=tk.Entry(window,textvariable=op,font=("Courier", 24))
-        self.e4.grid(row=1,column=3)
-        
-        count=tk.StringVar()
-        self.e5=tk.Entry(window,textvariable=count,font=("Courier", 24))
-        self.e5.grid(row=2,column=1)
-
-        self.b1=tk.Button(window,text="開始", width=20,font=("Courier", 16),command=self.add_command)
-        self.b1.grid(row=0,column=6)
-
-        self.b2= tk.Button(window,text="結束", width=20,font=("Courier", 16),command=self.detect_working)
-        self.b2.grid(row=1,column=6)
-
-        self.list1 = tk.Listbox(window, height=20,width=60)
-        self.list1.grid(row=3,column=0,rowspan=6,columnspan=2)
-        self.sb1= tk.Scrollbar(window)
-        self.sb1.grid(row=3,column=2,rowspan=10)
-
-        self.list1.configure(yscrollcommand= self.sb1.set)
-        self.sb1.configure(command= self.list1.yview)
-
-        self.b3 = tk.Button(window,text="鍵盤",width=20,font=("Courier", 16),command=self.callKeyboard)
-        self.b3.grid(column=6,row=3)
-        window.mainloop()
-        
-    def __init__(self):
-        print('tttt')
-
-a= detectWindow()
-a.window_initial()
+        added_thread.start()   
+window = Tk()
+detectWindow(window)
+window.mainloop()
